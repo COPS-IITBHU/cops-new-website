51a5612d6f1cde5d7415e404f7e60da86cdc7191
baditra-ops <baditrachoudhury@gmail.com>
2026-07-09 13:55:21 +0530

    feat(about-section): add AboutSection component to showcase IG details

--- src/app/verticals/ig/page.tsx
+++ src/app/verticals/ig/page.tsx
@@ -1,13 +1,14 @@
 import HeroSubsection from '@/components/layout/HeroSubsection';
 // import AchievementsSubsection from '@/components/layout/AchievementsSubsection';
 // import ProjectsSubsection from '@/components/layout/ProjectsSubsection';
-
+import AboutSection from '@/app/verticals/ig/AboutSection'
 export default function Home() {
   return (
     <div className='min-h-screen'>
       <HeroSubsection name='IG' />
       {/* <AchievementsSubsection /> */}
       {/* <ProjectsSubsection /> */}
+      <AboutSection />
     </div>
   );
 }
